c4e230f3987ba85a10a9ed45a2b282202c2a0d45
nobug <sudo_rm@163.com>
2025-01-16 22:09:11 +0800

    add some code

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import argparse
+import sys
+import os
+import getpass
+
+
+try:
+    from dotenv import load_dotenv
+
+    print("Loading .env file")
+    load_dotenv()
+    print("Loaded .env file\n")
+except Exception as e:
+    print(f"Error loading .env file: {e}")
+    sys.exit(1)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
         description='Twiter Friends List Lookup Tool')
-    parser.add_argument('-u', '--user', type=str, help='username')
-    parser.add_argument('-p', '--passwd', type=str, help='password')
+    parser.add_argument('-u', '--user', type=str,
+                        default=os.getenv("TWITTER_USERNAME"), help='Your twitter username.')
+    parser.add_argument('-p', '--password', type=str,
+                        default=os.getenv("TWITTER_PASSWORD"), help='Your twitter password')
 
     args = parser.parse_args()
     return args
@@ -35,6 +51,17 @@ def main():
 =======================================================
     """
     print(banner)
+
+    USER_UNAME = args.user
+    USER_PASSWORD = args.password
+
+    if USER_UNAME is None:
+        USER_UNAME = input("Twitter Username: ")
+
+    if USER_PASSWORD is None:
+        USER_PASSWORD = getpass.getpass("Enter Password: ")
+
+    print()
 
 
 if __name__ == "__main__":
---
+++ twitter_scraper.py
@@ -0,0 +1,81 @@
+import sys
+import os
+import time
+from fake_headers import Headers
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    StaleElementReferenceException,
+    WebDriverException,
+)
+
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.edge.service import Service as EdgeService
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+TWITTER_LOGIN_URL = "https://twitter.com/i/flow/login"
+
+
+class TwitterScraper:
+    def __init__(self, username, password):
+        print("Initializing Twitter Scraper...")
+
+        self.username = username
+        self.password = password
+
+    def _get_driver(self):
+        print("Setup WebDriver...")
+        header = Headers().generate()["User-Agent"]
+
+        # browser_option = EdgeOptions()
+        browser_option = ChromeOptions()
+
+        browser_option.add_argument("--no-sandbox")
+        browser_option.add_argument("--disable-dev-shm-usage")
+        browser_option.add_argument("--ignore-certificate-errors")
+        browser_option.add_argument("--disable-gpu")
+        browser_option.add_argument("--log-level=3")
+        browser_option.add_argument("--disable-notifications")
+        browser_option.add_argument("--disable-popup-blocking")
+        browser_option.add_argument("--user-agent={}".format(header))
+
+        # For Hiding Browser
+        browser_option.add_argument("--headless")
+
+        try:
+            print("Initializing ChromeDriver...")
+            driver = webdriver.Chrome(
+                options=browser_option,
+            )
+
+            print("WebDriver Setup Complete")
+            return driver
+        except WebDriverException:
+            try:
+                print("Downloading ChromeDriver...")
+                chromedriver_path = ChromeDriverManager().install()
+                chrome_service = ChromeService(
+                    executable_path=chromedriver_path)
+
+                print("Initializing ChromeDriver...")
+                driver = webdriver.Chrome(
+                    service=chrome_service,
+                    options=browser_option,
+                )
+
+                print("WebDriver Setup Complete")
+                return driver
+            except Exception as e:
+                print(f"Error setting up WebDriver: {e}")
+                sys.exit(1)
+
+    def login(self):
+        print()
+        print("Logging in to Twitter...")
+
+        self.driver.get(TWITTER_LOGIN_URL)
+        time.sleep(3)
